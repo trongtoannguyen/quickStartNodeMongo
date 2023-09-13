b8e677269e1f01cf681214a063cf929b3ece4f34
JackieChainnn <trongtoan1609ht@gmail.com>
2023-09-13 09:45:41 +0700

    listCollections()

--- collections.js
+++ collections.js
@@ -1,16 +1,37 @@
 const MongoClient = require('./connection');
 
 //List Collections
-async function listCollections(client) {
-    const dbo = await client.db('sample_airbnb');
-    const collectionsList = await dbo.listCollections().toArray();
-    console.log('Collections: ');
-    collectionsList.forEach(cols => console.log(` - ${cols.name}`));
+const listCollections = async (client) => {
+    const dbo = await client.db('sample_database');
+    try {
+        const collectionsList = await dbo.listCollections().toArray();
+        console.log('Collections: ');
+        for (const collectionInfo of collectionsList) {
+            const collectionName = collectionInfo.name;
+            console.log(` - ${collectionName}`);
+        }
+    } catch (error) {
+        console.error('Error listing collections:', error);
+    }
+}
+
+//Create Collection
+async function createCollection(client, nameOfCollection) {
+    const dbo = await client.db('sample_database');
+    try {
+        const collection = await dbo.createCollection(nameOfCollection);
+        console.log(`New collection named ${collection.collectionName} was created in ${collection.dbName}`);
+    } catch (error) {
+        throw error.message;
+    } finally {
+        await MongoClient.close();
+    }
 }
 
 async function main() {
     try {
         await listCollections(MongoClient);
+        await createCollection(MongoClient, 'Car');
     } catch (e) {
         console.error(e);
     } finally {
