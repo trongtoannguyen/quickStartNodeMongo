7b901f897f3fdbb8d0d1e9387ad5048bbdf272c6
JackieChainnn <trongtoan1609ht@gmail.com>
2023-09-17 23:08:48 +0700

    aggregation with $project

--- aggregation.js
+++ aggregation.js
@@ -6,6 +6,38 @@ of data transformations on the documents in a collection.
  */
 
 
+// $match $sort and $project
+async function printMatch(client, balanceGreater) {
+    const pipeline = [
+        {
+            $match: {
+                balance: { $gt: balanceGreater }
+            }
+        }, {
+            $sort: {
+                balance: -1
+            }
+        }, {
+            $project: {
+                _id: 0,
+                accountId: 1,
+                accountHolder: 1,
+                account_type: 1,
+                balance: 1,
+                // balance in Great British Pound
+                gbp_balance: {
+                    $divide: ['$balance', 1.3]
+                }
+            }
+        },
+    ];
+    const dbo = client.db('bank').collection('accounts');
+    const aggCursor = await dbo.aggregate(pipeline);
+    for await (const doc of aggCursor)
+        console.log(doc);
+}
+
+// $match $sort and $group
 async function printAccountMatch(client, balanceGreater) {
     const pipeline = [
         // Stage 1: All account match balance > given amount.
@@ -72,7 +104,7 @@ async function main() {
         await client.connect();
         console.log('Connection status: [Established]');
 
-        await printAccountMatch(client, 9000);
+        await printMatch(client, 900);
     } catch (error) {
         console.error(error);
     } finally {
